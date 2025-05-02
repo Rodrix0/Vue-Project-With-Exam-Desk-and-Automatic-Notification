9868fbbe2e9fb4039b73a7d4cd69cc7250c7a487
Rodri <rodrigomagallanes0@gmail.com>
2025-05-02 12:34:31 -0300

    para probr

---
+++ mi-vue-app/eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import vue from 'eslint-plugin-vue';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.vue', '**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      vue,
+    },
+    rules: {
+      'vue/no-unused-vars': 'warn',
+    },
+  },
+];
